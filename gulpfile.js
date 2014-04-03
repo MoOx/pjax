e09c16449198fd91f9e63a7c10a3d4384c99ddda
Maxime Thirouin <m@moox.io>
2014-04-03 07:07:34 +0200

    Update gulpfile from my boilerplate & relocate pjax.js into src folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,56 @@
-///
 var pkg = require("./package.json")
   , gulp = require("gulp")
   , plumber = require("gulp-plumber")
 
 ///
-// Lint JS
+// JS Lint
 ///
 var jshint = require("gulp-jshint")
-  , jsFiles = [".jshintrc", "*.json", "*.js"]
+  , jsonFiles = [".jshintrc", "*.json"]
+  , jsFiles = ["*.js", "src/**/*.js"]
 gulp.task("scripts.lint", function() {
-  gulp.src(jsFiles)
+  gulp.src([].concat(jsonFiles).concat(jsFiles))
     .pipe(plumber())
     .pipe(jshint(".jshintrc"))
     .pipe(jshint.reporter("jshint-stylish"))
 })
 
+///
+// JS Code Sniffing
+///
 var jscs = require("gulp-jscs")
 gulp.task("scripts.cs", function() {
-  gulp.src("*.js")
+  gulp.src(jsFiles)
     .pipe(plumber())
     .pipe(jscs())
 })
 
+// JS Alias
 gulp.task("scripts", ["scripts.lint", "scripts.cs"])
 
+///
+// Watch
+///
 gulp.task("watch", function() {
-  gulp.watch([jsFiles], ["scripts"])
+  gulp.watch(jsFiles, ["scripts"])
 })
 
-gulp.task("default", ["scripts", "watch"])
+///
+// Publish gh-branch
+///
+var buildBranch = require("buildbranch")
+gulp.task("publish", ["test"], function(cb) {
+  buildBranch({folder: "src"}
+  , function(err) {
+      if (err) {
+        throw err
+      }
+      console.log(pkg.name + " published.")
+      cb()
+    })
+})
+
+// Aliases
+gulp.task("build", ["scripts"])
+gulp.task("test", ["build"])
+gulp.task("default", ["test", "watch"])
