4404f21077cbf71941b4eaf8ad283cc6daac2999
Maxime Thirouin <m@moox.io>
2014-05-04 08:33:59 +0200

    Test clone method

---
+++ src/scripts/lib/clone.js
@@ -0,0 +1,12 @@
+module.exports = function(obj) {
+  if (null === obj || "object" != typeof obj) {
+    return obj
+  }
+  var copy = obj.constructor()
+  for (var attr in obj) {
+    if (obj.hasOwnProperty(attr)) {
+      copy[attr] = obj[attr]
+    }
+  }
+  return copy
+}
