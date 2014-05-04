52d7971dc53aeba02a55dfad64ac79e90a92cbec
Maxime Thirouin <m@moox.io>
2014-05-04 08:45:22 +0200

    Test xhr request

---
+++ src/scripts/lib/request.js
@@ -0,0 +1,19 @@
+module.exports = function(location, callback) {
+  var request = new XMLHttpRequest()
+
+  request.onreadystatechange = function() {
+    if (request.readyState === 4) {
+      if (request.status === 200) {
+        callback(request.responseText, request)
+      }
+      else {
+        callback(null, request)
+      }
+    }
+  }
+
+  request.open("GET", location + (!/[?&]/.test(location) ? "?": "&") + (new Date().getTime()), true)
+  request.setRequestHeader("X-Requested-With", "XMLHttpRequest")
+  request.send(null)
+  return request
+}
---
+++ tests/scripts/lib/request.js
@@ -0,0 +1,16 @@
+var tape = require("tape")
+
+var request = require("../../../src/scripts/lib/request.js")
+
+tape("test xhr request", function(t) {
+  var xhr = request("https://api.github.com/", function(result) {
+    try {
+      result = JSON.parse(result)
+    }
+    catch (e) {
+      t.fail("xhr doesn't get a JSON response")
+    }
+    t.same(typeof result, "object", "xhr request get a result")
+    t.end()
+  })
+})
