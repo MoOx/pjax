b1de220555c04ac1b5684cbc5cb81e8aa2e2e431
Maxime Thirouin <m@moox.io>
2014-05-04 09:13:42 +0200

    Drop fireEvent since we are supporting IE10

--- src/scripts/lib/events/trigger.js
+++ src/scripts/lib/events/trigger.js
@@ -4,27 +4,13 @@ module.exports = function(els, events) {
   events = (typeof events === "string" ? events.split(" ") : events)
 
   events.forEach(function(e) {
-    var event
-    if (document.createEvent) {
-      event = document.createEvent("HTMLEvents")
-      event.initEvent(e, true, true)
-    }
-    else {
-      event = document.createEventObject()
-      event.eventType = e
-    }
-
+    var event // = new CustomEvent(e) // doesn't everywhere yet
+    event = document.createEvent("HTMLEvents")
+    event.initEvent(e, true, true)
     event.eventName = e
 
-    if (document.createEvent) {
-      forEachEls(els, function(el) {
-        el.dispatchEvent(event)
-      })
-    }
-    else {
-      forEachEls(els, function(el) {
-        el.fireEvent("on" + event.eventType, event)
-      })
-    }
+    forEachEls(els, function(el) {
+      el.dispatchEvent(event)
+    })
   })
 }
