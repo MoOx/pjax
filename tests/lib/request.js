7cdcf3c887bbbe13afeb1fa227a0886bb40dcf44
Bart Nagel <bart@tremby.net>
2016-06-24 01:40:33 -0700

    Refactor request test to allow further tests to be added

--- tests/lib/request.js
+++ tests/lib/request.js
@@ -3,14 +3,17 @@ var tape = require("tape")
 var request = require("../../lib/request.js")
 
 tape("test xhr request", function(t) {
-  request("https://api.github.com/", function(result) {
-    try {
-      result = JSON.parse(result)
-    }
-    catch (e) {
-      t.fail("xhr doesn't get a JSON response")
-    }
-    t.same(typeof result, "object", "xhr request get a result")
-    t.end()
+  t.test("- request is made and gets a result", function(t) {
+    request("https://api.github.com/", function(result) {
+      try {
+        result = JSON.parse(result)
+      }
+      catch (e) {
+        t.fail("xhr doesn't get a JSON response")
+      }
+      t.same(typeof result, "object", "xhr request get a result")
+      t.end()
+    })
   })
+  t.end()
 })
