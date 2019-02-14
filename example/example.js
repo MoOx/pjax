3c1a4b2e187bc2e8c9401e17d8a945518a7e9153
BehindTheMath <9314934+BehindTheMath@users.noreply.github.com>
2019-02-13 22:26:57 -0500

    Switch linting to ESLint and Prettier (#191)
    * Switch linting to ESLint and Prettier
    * Clean up config
    * Prettier fixes

--- example/example.js
+++ example/example.js
@@ -1,51 +1,49 @@
 /* global Pjax */
 var pjax;
 var initButtons = function() {
-  var buttons = document.querySelectorAll("button[data-manual-trigger]")
+  var buttons = document.querySelectorAll("button[data-manual-trigger]");
 
   if (!buttons) {
-    return
+    return;
   }
 
   // jshint -W083
   for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function(e) {
-      var el = e.currentTarget
+      var el = e.currentTarget;
 
       if (el.getAttribute("data-manual-trigger-override") === "true") {
         // Manually load URL with overridden Pjax instance options
-        pjax.loadUrl("/example/page2.html", {cacheBust: false})
-      }
-      else
-      {
+        pjax.loadUrl("/example/page2.html", { cacheBust: false });
+      } else {
         // Manually load URL with current Pjax instance options
-        pjax.loadUrl("/example/page2.html")
+        pjax.loadUrl("/example/page2.html");
       }
-    })
+    });
   }
   // jshint +W083
-}
+};
 
-console.log("Document initialized:", window.location.href)
+console.log("Document initialized:", window.location.href);
 
 document.addEventListener("pjax:send", function() {
-  console.log("Event: pjax:send", arguments)
-})
+  console.log("Event: pjax:send", arguments);
+});
 
 document.addEventListener("pjax:complete", function() {
-  console.log("Event: pjax:complete", arguments)
-})
+  console.log("Event: pjax:complete", arguments);
+});
 
 document.addEventListener("pjax:error", function() {
-  console.log("Event: pjax:error", arguments)
-})
+  console.log("Event: pjax:error", arguments);
+});
 
 document.addEventListener("pjax:success", function() {
-  console.log("Event: pjax:success", arguments)
+  console.log("Event: pjax:success", arguments);
 
   // Init page content
-  initButtons()
-})
+  initButtons();
+});
 
 document.addEventListener("DOMContentLoaded", function() {
   // Init Pjax instance
@@ -53,9 +51,9 @@ document.addEventListener("DOMContentLoaded", function() {
     elements: [".js-Pjax"],
     selectors: [".body", "title"],
     cacheBust: true
-  })
-  console.log("Pjax initialized.", pjax)
+  });
+  console.log("Pjax initialized.", pjax);
 
   // Init page content
-  initButtons()
-})
+  initButtons();
+});
